93cdf30a0f7a0c14d306bddbf9f46b912884d942
林宣安 <jasonlin119@gmail.com>
2024-08-01 14:05:44 +0800

    Update board3.py

--- dash_flask/dashboard/board3.py
+++ dash_flask/dashboard/board3.py
@@ -1,10 +1,9 @@
 from dash import html, dcc, Input, Output, State
 import pandas as pd
-import dash
 import dash_bootstrap_components as dbc
-
+import dash
 from sqlalchemy import create_engine
-import os
+
 
 app1 = dash.Dash(__name__, requests_pathname_prefix='/dashboard/app3/')
 app1.title = '太陽能系統計算器'
